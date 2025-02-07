f23a12b4cef6f9862bf50ee669de968d2f594700
haileed04 <haileeisabella04@gmail.com>
2025-02-07 21:14:24 +0000

    implemented sparse matrices

--- quantum_computing_project/quantum_computing_project/register/register.py
+++ quantum_computing_project/quantum_computing_project/register/register.py
@@ -38,7 +38,8 @@ class Register(Simulator):
         if self.n_qubits == 0:
             self._reg = None
         else:
-            self._reg = reduce(self.tensor, states)
+            # self._reg = reduce(self.tensor, states)
+            self._reg = reduce(self.sparse_tensor, states)
 
         Simulator.registers.append(self)
     
@@ -107,11 +108,13 @@ class Register(Simulator):
         Raises:
             TypeError: If the other object is not an instance of the Register class.
         """
-        zero = np.array([[1], [0]])
+        # zero = np.array([[1], [0]])
+        zero = sps.coo_matrix(([1], ([0], [0])), shape=(2, 1))
         if isinstance(other, Register):
             new_n_bits = self.n_qubits + other.n_qubits
             newReg = Register(new_n_bits, [zero for _ in range(new_n_bits)])
-            newReg.reg = self.tensor(self.reg, other.reg)
+            # newReg.reg = self.tensor(self.reg, other.reg)
+            newReg.reg = self.sparse_tensor(self.reg, other.reg)
             return newReg
         else:
             raise TypeError("The objects added must be instances of the same class.")
--- quantum_computing_project/test/test_register/test_register.py
+++ quantum_computing_project/test/test_register/test_register.py
@@ -1,14 +1,39 @@
+import pytest
 import numpy as np
 import scipy.sparse as sps
 from quantum_computing_project.register import Register
 
 class TestRegister():
 
+    # NON-SPARSE MATRICES TEST
+    # @classmethod
+    # def setup_class(cls):
+    #     """Sets up zero and one state so they can be used throughout the class."""
+    #     cls.zero = np.array([[1], [0]])
+    #     cls.one = np.array([[0], [1]]) 
+
+    # def test_add(self):
+    #     """
+    #     Tests the addition of two quantum registers.
+
+    #     This test checks whether adding two quantum registers correctly creates 
+    #     a new register with the combined number of qubits and appropriate states.
+
+    #     Asserts:
+    #         - The concatenated register's quantum state matches the expected quantum state.
+    #     """
+    #     r1 = Register(n_qubits=2, states=[self.zero, self.zero])
+    #     r2 = Register(n_qubits=3, states=[self.zero, self.zero, self.zero])
+    #     result_register = r1 + r2
+    #     expected_register = Register(5, [self.zero, self.zero, self.zero, self.zero, self.zero])
+        
+    #     np.testing.assert_array_equal(result_register._reg, expected_register._reg)
+
     @classmethod
     def setup_class(cls):
-        """Sets up zero and one state so they can be used throughout the class."""
-        cls.zero = np.array([[1], [0]])
-        cls.one = np.array([[0], [1]]) 
+        """Sets up (sparse) zero and one states so they can be used throughout the class."""
+        cls.zero = sps.coo_matrix(([1], ([0], [0])), shape=(2, 1))
+        cls.one = sps.coo_matrix(([1], ([1], [0])), shape=(2, 1))
 
     def test_add(self):
         """
@@ -25,7 +50,18 @@ class TestRegister():
         result_register = r1 + r2
         expected_register = Register(5, [self.zero, self.zero, self.zero, self.zero, self.zero])
         
-        np.testing.assert_array_equal(result_register._reg, expected_register._reg)
+        np.testing.assert_array_equal(result_register._reg.toarray(), expected_register._reg.toarray())
 
+    def test_add_err(self):
+        """
+        Tests the addition of a quantum register with an invalid object (str).
 
+        Ensures that adding a Register to a non-Register object raises a TypeError.
 
+        Asserts:
+            - A TypeError is raised.
+        """
+        r1 = Register(n_qubits=2, states=[self.zero, self.zero])
+
+        with pytest.raises(TypeError):
+            r1 + "invalid_object"

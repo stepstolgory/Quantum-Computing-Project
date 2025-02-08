425fe23f26a1205d7af1d2e06c549f5dd9196521
haileed04 <haileeisabella04@gmail.com>
2025-02-08 17:43:15 +0000

    fixed docstring

--- quantum_computing_project/simulator/simulator.py
+++ quantum_computing_project/simulator/simulator.py
@@ -6,6 +6,9 @@ from quantum_computing_project.gate import Gate
 from quantum_computing_project.operations import Operations
 
 class Simulator:
+    """
+    The Simulator class is where Deutsch-Josza, Grover's etc. will be implemented.
+    """
 
     def __init__(self, registers):
         self.registers = registers
